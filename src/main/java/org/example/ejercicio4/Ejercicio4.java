da3b5c19f1d55a0636a10144444341a0bf9110d5
lraffinetti <lraffinetti@fi.uba.ar>
2026-04-29 23:57:37 -0300

    Hago ejercicio 4.

--- src/main/java/org/example/ejercicio4/Ejercicio4.java
+++ src/main/java/org/example/ejercicio4/Ejercicio4.java
@@ -1,7 +1,58 @@
 package org.example.ejercicio4;
+import java.util.ArrayList;
+import java.util.List;
+
+class Jugador {
+    String nombre;
+    String posicion;
+
+    public Jugador(String nombre, String posicion){
+        this.nombre = nombre;
+        this.posicion = posicion;
+    }
+
+    public int getSueldo(){
+
+        if (posicion == "Delantero"){
+            return 100000;
+        }else if(posicion == "MedioCampista"){
+            return 70000;
+        }else if(posicion == "Defensor"){
+            return 50000;
+        }else if(posicion == "Arquero"){
+            return 30000;
+        }else{
+            throw new RuntimeException("Posicion no encontrada.");
+        }
+        
+    }
+}
+class FCBarcelona {
+    List<Jugador> jugadores;
+
+    public FCBarcelona(){
+        jugadores = new ArrayList<>();
+    }
+
+    public void sueldosJugadores(){
+        for(Jugador jugador : jugadores){
+            System.out.println(jugador.getSueldo());
+        }
+    }
+
+}
 
 public class Ejercicio4 {
     public static void main(String[] args){
+        FCBarcelona equipo = new FCBarcelona();
 
+        equipo.jugadores.add(new Jugador("Lewandowski", "Delantero"));
+        equipo.jugadores.add(new Jugador("Pedri", "MedioCampista"));
+        equipo.jugadores.add(new Jugador("Gavi", "MedioCampista"));
+        equipo.jugadores.add(new Jugador("Araujo", "Defensor"));
+        equipo.jugadores.add(new Jugador("Kounde", "Defensor"));
+        equipo.jugadores.add(new Jugador("Ter Stegen", "Arquero"));
+
+        equipo.sueldosJugadores();
     }
 }
